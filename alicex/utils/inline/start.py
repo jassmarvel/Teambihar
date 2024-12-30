829079bd48f885f46d0d6d19701d799a38a80dcc
jassmarvel <159693800+jassmarvel@users.noreply.github.com>
2024-12-30 21:39:57 +0530

    Update start.py

--- alicex/utils/inline/start.py
+++ alicex/utils/inline/start.py
@@ -24,11 +24,14 @@ def private_panel(_):
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
-      #  [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+        [
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.DEV_ID),
+            InlineKeyboardButton(text=_["S_B_10"], user_id=config.OWNER_ID),
+        ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
     ]
     return buttons
